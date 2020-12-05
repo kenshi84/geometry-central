7d02f84ea2c515eb305e45fdb33a95c38c06ce2c
Kenshi Takayama <kenshi84@gmail.com>
2020-12-05 16:28:00 +0900

    combining_hash_functions: move hash_combine() to the global scope

--- include/geometrycentral/utilities/combining_hash_functions.h
+++ include/geometrycentral/utilities/combining_hash_functions.h
@@ -11,16 +11,16 @@
 // See https://stackoverflow.com/questions/7110301/generic-hash-for-tuples-in-unordered-map-unordered-set
 // and elsewhere
 
-namespace std {
-
 // Combinie hash values in a not-completely-evil way
 // (I think this is strategy boost uses)
-namespace {
 template <class T>
 inline void hash_combine(std::size_t& seed, T const& v) {
   seed ^= std::hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
 
+namespace std {
+
+namespace {
 // Recursive template code derived from Matthieu M.
 template <class Tuple, size_t Index = std::tuple_size<Tuple>::value - 1>
 struct HashValueImpl {
